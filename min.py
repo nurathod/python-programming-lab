c867d6f09eb7cfbc87f5cda6d6b58255943a935a
nurathod <46518399+nurathod@users.noreply.github.com>
2019-01-16 14:08:44 +0530

    Add files via upload

---
+++ min.py
@@ -0,0 +1,15 @@
+x=int(input('time in hours'))
+y=x*30
+print(y)
+z=int(input('time in minutes'))
+p=z/2
+print('number degree')
+print(p)
+v=p+y
+print(v)
+c=int(input('enter time in second'))
+k=c/20
+print(k)
+f=p+y+k
+print(f)
+
